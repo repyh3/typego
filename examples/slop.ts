d744821972759960082a980de3d7fec4a4c608a5
Repyh <cjho88793@gmail.com>
2026-01-20 20:46:20 +0700

    test: add bad style example for copilot review

---
+++ examples/slop.ts
@@ -0,0 +1,19 @@
+import { Println } from "go:fmt";
+
+// This file is an example of bad coding style that should be caught by Copilot.
+// I have created this file to demonstrate the review capabilities.
+
+// This function prints a message
+function hello() {
+    // I am setting the message variable here
+    const message = "Hello World";
+
+    // Now I will print the message to the console
+    Println(message);
+
+    // REST ASSURED that this function works correctly.
+    // HAPPY CODING!
+}
+
+// Here is the main function
+hello();
